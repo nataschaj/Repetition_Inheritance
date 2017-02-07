eae3018cd15fc1679207ea1a58f4a1130e7c5d7d
natsachaj <natascha_jakobsen@hotmail.com>
2017-02-07 11:27:30 +0100

    Add project files.

---
+++ RepetitionInheritance/Bil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionInheritance
+{
+    public class Bil
+    {
+        public int BilPrisExAfgift { get; set; }
+        public int KøbsÅr { get; set; }
+        public string Mærke { get; set; }
+        public string RegistreringsNr { get; set; }
+        public int KmPrLiter { get; set; }
+
+        public Bil()
+        {
+            this.KøbsÅr = 2016;
+            this.Mærke = "Audi";
+            this.BilPrisExAfgift = 100;
+            this.KmPrLiter = 600;
+        }
+
+        public virtual int RegistreringsAfgift()
+        {
+            
+        }
+
+        public int TotalPris()
+        {
+
+        }
+
+        public int HalvÅrligEjerafgift()
+        {
+            return 1000;
+        }
+    }
+}
